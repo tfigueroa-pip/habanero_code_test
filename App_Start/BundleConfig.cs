1980e28910ff253bd08868fab6cb3f5be822a708
tfigueroa-pip <timothy@psalmsimageplus.com>
2023-03-29 18:38:33 +0800

    move scripts and styles out of index

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -14,18 +14,25 @@ namespace HabaneroCodeTest
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/angular.min.js"));
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/headerscripts").Include(
+                        "~/Scripts/angular.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/footerscripts").Include(
+                        "~/Scripts/homeapp.js"));
         }
     }
 }
---
+++ Scripts/homeapp.js
@@ -0,0 +1,23 @@
+﻿var app = angular.module('gamesAngularList', []);
+var gameTypes = gameModel.map(getGameTypes);
+function getGameTypes(item) {
+    return item.GameTypeName;
+};
+gameTypes = [...new Set(gameTypes)];
+var gameTypes2 = gameTypes.map(function (item, index) {
+    return { 'name': item, 'index': index };
+});
+console.log(gameModel);
+app.controller('gamesAngularController', function ($scope) {
+    $scope.angularModel = gameModel;
+    $scope.logoTypes = {
+        'Circle Flat': 'circleflat',
+        'Oval Flat': 'ovalflat',
+        'Rectangle': 'rectangle',
+        'Square': 'square'
+    };
+    $scope.gameTypes = gameTypes;
+    $scope.orderByThis = function (x) {
+        $scope.gameOrdering = x;
+    };
+});
